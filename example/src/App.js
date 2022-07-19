1bbda48a37f5d7c1ac0ec40726d3669d48f272a0
Mustafa Boşnak <49624772+mustafabosnak@users.noreply.github.com>
2022-07-19 13:10:43 +0300

    Npm test

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
-import { ExampleComponent } from 'mb-button-test'
+import { Button } from 'mb-button-test'
 import 'mb-button-test/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+const App = () => (
+  <>
+    <Button text="Test Button" type="primary" color="info" />
+    <Button text="Test Button" type="default" color="info" />
+    <Button text="Test Button" type="dashed" color="info" />
+    <Button text="Test Button" type="text" color="info" />
+    <Button text="Test Button" type="link" color="info" />
+    <br/>
+    <br/>
+    <Button text="Test Button" type="primary" color="success" />
+    <Button text="Test Button" type="default" color="success" />
+    <Button text="Test Button" type="dashed" color="success" />
+    <Button text="Test Button" type="text" color="success" />
+    <Button text="Test Button" type="link" color="success" />
+    <br/>
+    <br/>
+    <Button text="Test Button" type="primary" color="danger" />
+    <Button text="Test Button" type="default" color="danger" />
+    <Button text="Test Button" type="dashed" color="danger" />
+    <Button text="Test Button" type="text" color="danger" />
+    <Button text="Test Button" type="link" color="danger" />
+    <br/>
+    <br/>
+    <Button text="Test Button" type="primary" color="warning" />
+    <Button text="Test Button" type="default" color="warning" />
+    <Button text="Test Button" type="dashed" color="warning" />
+    <Button text="Test Button" type="text" color="warning" />
+    <Button text="Test Button" type="link" color="warning" />
+  </>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props) => {
+  const btntype = props.type ? props.type : "primary";
+  const btncolor = props.color ? props.color : "info";
+  let styleValue = Object.values(styles);
+  let styleKey = Object.keys(styles);
+  const styleArr = styleKey.reduce((acc, value, i) => (acc[value] = styleValue[i], acc), {});
+  return <button className={styleArr[btntype] + " " + styleArr[btncolor]} {...props}>{props.text}</button>
 }
